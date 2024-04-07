0a7947064672569ba87017b3496c780a16dd5def
M0RG0H <165952277+M0RG0H@users.noreply.github.com>
2024-04-07 11:33:05 -0700

    Add files via upload

--- Neural_Network.h
+++ Neural_Network.h
@@ -12,6 +12,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <iomanip>
+#include <iostream>
 
 
 using namespace std;
@@ -24,22 +25,21 @@ class NeuralNetwork
 
         vector<int> topology;
 
-        vector<vector<double>> cacheLayers;
-        vector<vector<double>> layers;
+        vector<vector<double>>* cacheLayers;
+        vector<vector<double>>* layers;
 
-        vector<vector<vector<double>>> weights;
-        vector<vector<double>> biases;
+        vector<vector<vector<double>>>* weights;
+        vector<vector<double>>* biases;
 
         double trainingRate;
 
 
-        double sigmoid(double x);
-        double sigmoid_derivative(double x);
+        double sigmoid(double x) const;
+        double sigmoid_derivative(double x) const;
 
-        void propogateForward(vector<double>& input);
+        void propogateForward(const vector<double>& input);
 
-        void propogateBackwards(const vector<double>& output, vector<vector<vector<double>>>& wG, vector<vector<double>>& bG, int num_training_examples);
-        void updateWeightsAndBiases(vector<vector<vector<double>>> wGradient, vector<vector<double>> bGradient);
+        void propogateBackwards(const vector<double>& output, int num_training_examples);
 
         vector<double> str_to_double_vec(const string& tmps);
 
@@ -50,6 +50,12 @@ class NeuralNetwork
         {
             vector<double> pixels; // Input
             vector<double> answer; // Expected output
+
+            digit(vector<double> i, vector<double> o)
+            : pixels(i), answer(o)
+            {
+                
+            }
         };
 
 
@@ -69,35 +75,40 @@ class NeuralNetwork
 // Code Break
 //
 
-double NeuralNetwork::sigmoid(double x)
+double NeuralNetwork::sigmoid(double x) const
 {
 	return 1 / (1 + exp(-x));
 }
 
-double NeuralNetwork::sigmoid_derivative(double x)
+double NeuralNetwork::sigmoid_derivative(double x) const
 {
 	return sigmoid(x) * (1 - sigmoid(x));
 }
 
-void NeuralNetwork::propogateForward(vector<double>& input)
+void NeuralNetwork::propogateForward(const vector<double>& input)
 {
-    layers[0] = input;
+    for (int i = 0; i < topology[i]; i++) {
+        layers->at(0)[i] = input[i];
+        cacheLayers->at(0)[i] = input[i];
+    }
 
     for (int i = 1; i < topology.size(); i++) {
         for (int j = 0; j < topology[i]; j++) {
+            layers->at(i)[j] = 0;
+
             for (int m = 0; m < topology[i - 1]; m++) {
-                layers[i][j] += layers[i - 1][m] * weights[i - 1][j][m];
+                layers->at(i)[j] += layers->at(i - 1)[m] * weights->at(i - 1)[j][m];
             }
 
-            layers[i][j] += biases[i][j];
+            layers->at(i)[j] += biases->at(i - 1)[j];
 
-            cacheLayers[i][j] = layers[i][j];
-            layers[i][j] = sigmoid(layers[i][j]);
+            cacheLayers->at(i)[j] = layers->at(i)[j];
+            layers->at(i)[j] = sigmoid(layers->at(i)[j]);
         }
     }
 }
 
-void NeuralNetwork::propogateBackwards(const vector<double>& output, vector<vector<vector<double>>>& wG, vector<vector<double>>& bG, int num_training_examples)
+void NeuralNetwork::propogateBackwards(const vector<double>& output, int num_training_examples)
 {
     for (int i = 0; i < topology.size() - 1; i++) {
         for (int j = 0; j < topology[i + 1]; j++) {
@@ -106,45 +117,45 @@ void NeuralNetwork::propogateBackwards(const vector<double>& output, vector<vect
 
                 if (i == topology.size() - 2) {
                     val += trainingRate;
-                    val *= 2 * sigmoid(output[j] - sigmoid(cacheLayers[i + 1][j])) * sigmoid_derivative(cacheLayers[i + 1][j]) * sigmoid_derivative(cacheLayers[i][k]);
+                    val *= 2 * sigmoid(output[j] - sigmoid(cacheLayers->at(i + 1)[j])) * sigmoid_derivative(cacheLayers->at(i + 1)[j]) * sigmoid_derivative(cacheLayers->at(i)[k]);
                 } else {
                     for (int g = 0; g < topology[topology.size() - 1]; g++) {
                         double val2 = 0;
-                        val2 += trainingRate * 2 * (output[g] - sigmoid(cacheLayers[topology.size() - 1][g])) * -1 * sigmoid_derivative(cacheLayers[topology.size() - 1][g]);
+                        val2 += trainingRate * 2 * (output[g] - sigmoid(cacheLayers->at(topology.size() - 1)[g])) * -1 * sigmoid_derivative(cacheLayers->at(topology.size() - 1)[g]);
 
-                        for (int h = topology.size() - 2; h > i + 1; h++) {
+                        for (int h = topology.size() - 2; h > i + 1; h--) {
                             for (int m = 0; m < topology[h]; m++) {
-                                val2 *= sigmoid_derivative(cacheLayers[h][m]);
+                                val2 *= sigmoid_derivative(cacheLayers->at(h)[m]);
                             }
                         }
 
-                        val2 *= sigmoid_derivative(cacheLayers[i][k]);
+                        val2 *= sigmoid_derivative(cacheLayers->at(i)[k]);
 
                         val += val2;
                     }
                 }
 
-                wG[i][j][k] += val / num_training_examples;
+                weights->at(i)[j][k] -= val / num_training_examples;
             }
         }
     }
 
 
-    for (int i = 1; i < topology.size(); i++) {
+    for (int i = 0; i < topology.size() - 1; i++) {
         for (int j = 0; j < topology[i]; j++) {
             double val = 0;
 
             if (i == topology.size() - 1) {
                 val = trainingRate;
-                val *= 2 * (output[j] - sigmoid(cacheLayers[i][j])) * -1 * sigmoid_derivative(cacheLayers[i][j]);
+                val *= 2 * (output[j] - sigmoid(cacheLayers->at(i)[j])) * -1 * sigmoid_derivative(cacheLayers->at(i)[j]);
             } else {
                 for (int g = 0; g < topology[topology.size() - 1]; g++) {
                     double val2 = 0;
-                    val2 += trainingRate * 2 * (output[g] - sigmoid(cacheLayers[topology.size() - 1][g])) * -1 * sigmoid_derivative(cacheLayers[topology.size() - 1][g]);
+                    val2 += trainingRate * 2 * (output[g] - sigmoid(cacheLayers->at(topology.size() - 1)[g])) * -1 * sigmoid_derivative(cacheLayers->at(topology.size() - 1)[g]);
 
-                    for (int h = topology.size() - 2; h > i; h++) {
+                    for (int h = topology.size() - 2; h > i; h--) {
                         for (int k = 0; k < topology[h]; k++) {
-                            val2 *= sigmoid_derivative(cacheLayers[h][k]);
+                            val2 *= sigmoid_derivative(cacheLayers->at(h)[k]);
                         }
                     }
 
@@ -152,24 +163,7 @@ void NeuralNetwork::propogateBackwards(const vector<double>& output, vector<vect
                 }
             }
 
-            bG[i][j] += val / num_training_examples;
-        }
-    }
-}
-
-void NeuralNetwork::updateWeightsAndBiases(vector<vector<vector<double>>> wGradient, vector<vector<double>> bGradient)
-{
-    for (int i = 0; i < wGradient.size(); i++) {
-        for (int j = 0; j < wGradient[i].size(); j++) {
-            for (int m = 0; m < wGradient[i][j].size(); m++) {
-                weights[i][j][m] -= wGradient[i][j][m];
-            }
-        }
-    }
-
-    for (int i = 0; i < bGradient.size(); i++) {
-        for (int j = 0; j < bGradient[i].size(); j++) {
-            biases[i][j] -= bGradient[i][j];
+            biases->at(i)[j] -= val / num_training_examples;
         }
     }
 }
@@ -180,12 +174,15 @@ vector<double> NeuralNetwork::str_to_double_vec(const string& tmps)
 
     string tmp;
 
-    for (char c : tmps) {
-        if (c == ' ') {
+    for (int i = 0; i < tmps.size(); i++) {
+        if (tmps[i] == ' ') {
             v.push_back(stod(tmp));
             tmp.clear();
+        } else if (i == tmps.size() - 1) {
+            tmp += tmps[i];
+            v.push_back(stod(tmp));
         } else {
-            tmp += c;
+            tmp += tmps[i];
         }
     }
 
@@ -214,42 +211,51 @@ double NeuralNetwork::random_double() const
 //
 
 NeuralNetwork::NeuralNetwork(vector<int> topology, string savetofilename)
-: filename(savetofilename), topology(topology)
+: filename(savetofilename), topology(topology), cacheLayers(new vector<vector<double>>), layers(new vector<vector<double>>), weights(new vector<vector<vector<double>>>), biases(new vector<vector<double>>)
 {
     srand(time(nullptr));
 
     for (int i = 0; i < topology.size(); i++) {
-        layers.push_back(vector<double>(topology[i]));
-        cacheLayers.push_back(vector<double>(topology[i]));
-
-        if (i != 0) {
-            biases.push_back(vector<double>(topology[i]));
+        vector<double> lTemp;
+        vector<double> cacheTemp;
+        for (int j = 0; j < topology[i]; j++) {
+            double d = 0;
+            lTemp.push_back(d);
+            cacheTemp.push_back(d);
         }
+        layers->push_back(lTemp);
+        cacheLayers->push_back(cacheTemp);
 
-        if (i != topology.size() - 1) {
-            weights.push_back(vector<vector<double>>(topology[i], vector<double>(topology[i - 1])));
-        }
-    }
+        if (i > 0) {
+            vector<vector<double>> wTemp;
+            for (int j = 0; j < topology[i]; j++) {
+                vector<double> wTemp2;
+                for (int l = 0; l < topology[i - 1]; l++) {
+                    wTemp2.push_back(random_double());
+                }
 
-    for (auto& m : weights) {
-        for (auto& ws : m) {
-            for (auto& w : ws) {
-                w = random_double();
+                wTemp.push_back(wTemp2);
             }
-        }
-    }
 
-    for (auto& v : biases) {
-        for (auto& b : v) {
-            b = random_double();
+            vector<double> bTemp;
+            for (int j = 0; j < topology[i]; j++) {
+                bTemp.push_back(random_double());
+            }
+
+            weights->push_back(wTemp);
+            biases->push_back(bTemp);
         }
     }
 }
 
 NeuralNetwork::NeuralNetwork(string filename)
-: filename(filename)
+: filename(filename), cacheLayers(new vector<vector<double>>), layers(new vector<vector<double>>), weights(new vector<vector<vector<double>>>), biases(new vector<vector<double>>)
 {
     ifstream file(filename);
+
+    if (file.fail()) {
+        throw runtime_error("File " + filename + " failed to load properly");
+    }
 
     string top;
     getline(file, top);
@@ -269,8 +275,8 @@ NeuralNetwork::NeuralNetwork(string filename)
     }
 
     for (int i = 0; i < topology.size(); i++) {
-        layers.push_back(vector<double>(topology[i]));
-        cacheLayers.push_back(vector<double>(topology[i]));
+        layers->push_back(vector<double>(topology[i]));
+        cacheLayers->push_back(vector<double>(topology[i]));
     }
 
     string wghts;
@@ -300,13 +306,12 @@ NeuralNetwork::NeuralNetwork(string filename)
         }
     }
 
-
+    int layerIdx2 = 0;
 
     for (auto& s : v) {
         vector<vector<double>> v2;
 
         string temp2;
-        int layerIdx2 = 0;
         int countSpaces2 = 0;
 
         for (int i = 0; i < s.size(); i++) {
@@ -314,7 +319,6 @@ NeuralNetwork::NeuralNetwork(string filename)
                 countSpaces2++;
                 if (countSpaces2 == topology[layerIdx2]) {
                     countSpaces2 = 0;
-                    layerIdx++;
                     v2.push_back(str_to_double_vec(temp2));
                     temp2.clear();
                     continue;
@@ -328,7 +332,8 @@ NeuralNetwork::NeuralNetwork(string filename)
             }
         }
 
-        weights.push_back(v2);
+        weights->push_back(v2);
+        layerIdx2++;
     }
 
 
@@ -345,14 +350,14 @@ NeuralNetwork::NeuralNetwork(string filename)
             if (countSpaces3 == topology[layerIdx3]) {
                 countSpaces3 = 0;
                 layerIdx3++;
-                biases.push_back(str_to_double_vec(temp3));
+                biases->push_back(str_to_double_vec(temp3));
                 temp3.clear();
                 continue;
             }
             temp3 += biass[i];
         } else if (i == biass.size() - 1) {
             temp3 += biass[i];
-            biases.push_back(str_to_double_vec(temp3));
+            biases->push_back(str_to_double_vec(temp3));
         } else {
             temp3 += biass[i];
         }
@@ -363,53 +368,28 @@ NeuralNetwork::NeuralNetwork(string filename)
 
 vector<double> NeuralNetwork::runNetwork(digit example)
 {
-    if (example.pixels.size() != topology[0]) {
+    if ((example.pixels).size() != topology[0]) {
         throw runtime_error("NeuralNetwork::runNetwork can only accept digit parameters compatible with the input layer");
     }
 
     propogateForward(example.pixels);
 
-    return layers[topology.size() - 1];
+    return layers->at(topology.size() - 1);
 }
 
 void NeuralNetwork::train(vector<digit> trainingExamples, double trainingR)
 {
     trainingRate = trainingR;
 
-    vector<vector<vector<double>>> wG(topology.size() - 1);
-    vector<vector<double>> bG(topology.size() - 1);
-    for (int i = 0; i < topology.size(); i++) {
-        if (i != 0) {
-            bG.push_back(vector<double>(topology[i]));
-        }
-
-        if (i != topology.size() - 1) {
-            wG.push_back(vector<vector<double>>(topology[i], vector<double>(topology[i - 1])));
-        }
-    }
-
-    for (digit d : trainingExamples) {
-        if (d.pixels.size() != topology[0]) {
-            throw runtime_error("NeuralNetwork::runNetwork can only accept digit parameters compatible with the input layer");
+    for (auto& d : trainingExamples) {
+        if (d.pixels.size() != topology[0] || d.answer.size() != topology[topology.size() - 1]) {
+            throw runtime_error("NeuralNetwork::train can only accept digit parameters compatible with the input & output layers");
         }
 
         propogateForward(d.pixels);
 
-        propogateBackwards(d.answer, wG, bG, trainingExamples.size());
-    }
 
-    for (int i = 0; i < weights.size(); i++) {
-        for (int j = 0; j < weights[i].size(); j++) {
-            for (int m = 0; m < weights[i][j].size(); m++) {
-                weights[i][j][m] -= wG[i][j][m];
-            }
-        }
-    }
-
-    for (int i = 0; i < biases.size(); i++) {
-        for (int j = 0; j < biases[i].size(); j++) {
-            biases[i][j] -= bG[i][j];
-        }
+        propogateBackwards(d.answer, trainingExamples.size());
     }
 }
 
@@ -431,11 +411,11 @@ NeuralNetwork::~NeuralNetwork()
     file << "\n";
 
 
-    for (int i = 0; i < weights.size(); i++) {
-        for (int j = 0; j < weights[i].size(); j++) {
-            for (int k = 0; k < weights[i][j].size(); k++) {
-                file << fixed << setprecision(10) << weights[i][j][k];
-                if (k != weights[i][j].size() - 1) {
+    for (int i = 0; i < weights->size(); i++) {
+        for (int j = 0; j < weights->at(i).size(); j++) {
+            for (int k = 0; k < weights->at(i)[j].size(); k++) {
+                file << fixed << setprecision(10) << weights->at(i)[j][k];
+                if (!(k == weights->at(i)[j].size() - 1 && j == weights->at(i).size() - 1 && i == weights->size() - 1)) {
                     file << " ";
                 }
             }
@@ -444,17 +424,23 @@ NeuralNetwork::~NeuralNetwork()
     file << "\n";
 
 
-    for (int i = 0; i < biases.size(); i++) {
-        for (int j = 0; j < biases[i].size(); j++) {
-            file << fixed << setprecision(10) << biases[i][j];
-            if (j != biases[i].size() - 1) {
+    for (int i = 0; i < biases->size(); i++) {
+        for (int j = 0; j < biases->at(i).size(); j++) {
+            file << fixed << setprecision(10) << biases->at(i)[j];
+            if (!(j == biases->at(i).size() - 1 && i == biases->size() - 1)) {
                 file << " ";
             }
         }
+        
     }
 
 
     file.close();
+
+    delete[] layers;
+    delete[] cacheLayers;
+    delete[] weights;
+    delete[] biases;
 }
 
 
